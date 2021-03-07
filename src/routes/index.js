ae905459419f198ee7876b5d19d3427c11856d49
Mayank <mayankparikh21dec@gmail.com>
2021-03-07 23:32:30 +0530

    feat : Add routes for author, editor and publisher

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,15 +1,32 @@
 import { Router } from 'express';
-// const { registerUserType } = require('../controllers/adminController');
-// const { passport } = require('../lib/passport');
 import { graphqlHTTP } from 'express-graphql';
-import userSchema from '../querySchemas/userSchemas';
-import userRoots from '../rootResolver/userRoots';
+import passportLibrary from '../lib/passport';
+import schema from '../querySchemas';
+import roots from '../rootResolver';
 
 const router = Router();
 
+router.use('/author', passportLibrary.passport.authenticate('jwt-author', { session: false }), graphqlHTTP({
+  schema: schema.author,
+  rootValue: roots.author,
+  graphiql: true
+}));
+
+router.use('/editor', passportLibrary.passport.authenticate('jwt-editor', { session: false }), graphqlHTTP({
+  schema: schema.editor,
+  rootValue: roots.editor,
+  graphiql: true
+}));
+
+router.use('/publisher', passportLibrary.passport.authenticate('jwt-publisher', { session: false }), graphqlHTTP({
+  schema: schema.publisher,
+  rootValue: roots.publisher,
+  graphiql: true
+}));
+
 router.use('/', graphqlHTTP({
-  schema: userSchema,
-  rootValue: userRoots,
+  schema: schema.user,
+  rootValue: roots.user,
   graphiql: true
 }));
 
