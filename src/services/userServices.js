e8635479c86c68147bef3cbc4424119ce6152bbc
Mayank <mayankparikh21dec@gmail.com>
2021-03-07 20:46:46 +0530

    feat : Add methods for database interaction

---
+++ src/services/userServices.js
@@ -0,0 +1,39 @@
+import { hashSync } from 'bcryptjs';
+import moment from 'moment';
+import User from '../models/user';
+
+/**
+ * create new user in database
+ * @param {Object} userDetails - containing all required fields
+ * @param {String} userStatus - active/inactive user
+ */
+const addUser = async userDetails => {
+  const userObject = {
+    name: userDetails.name,
+    email: userDetails.email,
+    password: hashSync(userDetails.password, 10),
+    role: userDetails.role,
+    createdAt: moment().format(),
+    updatedAt: moment().format()
+  };
+
+  try {
+    const user = new User(userObject);
+    await user.save();
+    return user;
+  } catch (err) {
+    console.log(err.message);
+    throw new Error(`Unable to register user due to ${err.message}`);
+  }
+};
+
+/**
+ * Fetch user with matching email and return user object
+ * @param {String} email
+ */
+const findUserWithEmail = email => User.findOne({ email });
+
+export default {
+  addUser,
+  findUserWithEmail
+};
